2a49b38eb229083a1d1b8ddee361e6bfb006e9e0
Samtaupe <145565941+Samtaupe@users.noreply.github.com>
2025-02-07 09:53:46 +0100

    add method post to critique service

--- parc/src/app/Service/critique.service.ts
+++ parc/src/app/Service/critique.service.ts
@@ -8,11 +8,11 @@ import { MessageInterface } from '../Interface/message.interface';
   providedIn: 'root',
 })
 export class CritiqueService{
-
+  
   constructor(private dataService: DataService) {
-
+    
   }
-
+  
   public getCritiqueByIdAttraction(attraction_id : number) : Observable<CritiqueInterface[]> {
     const url = "http://127.0.0.1:5000/critique/" + attraction_id
     const data = this.dataService.getData(url);
@@ -23,5 +23,11 @@ export class CritiqueService{
     const url = "http://127.0.0.1:5000/critique"
     const data = this.dataService.getData(url);
     return data as Observable<CritiqueInterface[]>
+  }
+  
+  postCritique(critique: CritiqueInterface) {
+    const url = "http://127.0.0.1:5000/critique";
+    const data = this.dataService.postData(url, critique);
+    return data as Observable<MessageInterface>;
   }
 }

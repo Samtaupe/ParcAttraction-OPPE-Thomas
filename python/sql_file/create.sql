950d26da369506c9dac7e35af7b15edbeeeeaf3b
Samtaupe <145565941+Samtaupe@users.noreply.github.com>
2025-02-07 08:21:14 +0100

    Ajout table critique + exemples

--- python/sql_file/create.sql
+++ python/sql_file/create.sql
@@ -1,4 +1,24 @@
 INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Silver Star', 'Montagne russe', 3, 1);
 INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Montagne 8', 'Montagne russe', 4, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Blue Fire', 'Montagne russe', 4, 0);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Grand splash', 'Attraction sur eau', 1, 0);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Voltron', 'Montagne russe', 5, 1);
 
 INSERT INTO users (name, password) VALUES ('toto', 'toto');
+
+INSERT INTO critique (attraction_id, critique, note, prenom, nom, anonyme)
+VALUES 
+    (1, 'Une expérience incroyable ! Sensations fortes garanties.', 18, 'Lucas', 'Dupont', FALSE),
+    (1, 'Trop rapide à mon goût, mais bien pour les amateurs de vitesse.', 14, NULL, NULL, TRUE),
+
+    (2, 'J’ai adoré ! Un vrai défi pour les amateurs de sensations.', 19, 'Camille', 'Morel', FALSE),
+    (2, 'Un peu trop violent pour moi, mais sinon sympa.', 12, NULL, NULL, TRUE),
+
+    (3, 'Décevant, je m’attendais à plus de sensations.', 10, 'Julien', 'Bernard', FALSE),
+    (3, 'Bonne attraction mais un peu courte.', 15, NULL, NULL, TRUE),
+
+    (4, 'Très rafraîchissant par temps chaud, super agréable !', 17, 'Emma', 'Lefevre', FALSE),
+    (4, 'Pas assez de vagues à mon goût, trop tranquille.', 11, NULL, NULL, TRUE),
+
+    (5, 'Une des meilleures attractions du parc ! Incroyable !', 20, 'Mathis', 'Rousseau', FALSE),
+    (5, 'Grosse montée d’adrénaline, j’ai adoré !', 19, NULL, NULL, TRUE);
--- python/sql_file/init.sql
+++ python/sql_file/init.sql
@@ -1,4 +1,8 @@
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS attraction;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS critique;
+SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE attraction (
     attraction_id int auto_increment,
@@ -9,11 +13,27 @@ CREATE TABLE attraction (
     visible bool default true
 );
 
-DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
     users_id int auto_increment,
     primary key(users_id),
     name varchar(255) not null,
     password varchar(255) not null
 );
+
+
+CREATE TABLE critique (
+    critique_id INT AUTO_INCREMENT,
+    PRIMARY KEY (critique_id),
+    attraction_id INT NOT NULL,
+    FOREIGN KEY (attraction_id) REFERENCES attraction(attraction_id),
+    critique VARCHAR(255) NOT NULL,
+    note INT NOT NULL CHECK (note BETWEEN 0 AND 20),
+    prenom VARCHAR(255),
+    nom VARCHAR(255),
+    anonyme BOOLEAN DEFAULT FALSE,
+    
+    CONSTRAINT check_nom_prenom CHECK (
+        (anonyme = TRUE) OR (prenom IS NOT NULL AND nom IS NOT NULL)
+    )
+);
+
